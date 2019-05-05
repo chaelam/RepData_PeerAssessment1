a9b789d153d2f9164ba2242051edd123366fe916
chaelam <mmmiranda@globe.com.ph>
2019-05-05 18:45:28 +0800

    Plotted the average number of steps per interval, and determined the interval with the most steps

--- Code.R
+++ Code.R
@@ -16,3 +16,21 @@ total_steps_per_day<-with(activity,aggregate(steps,list(date),sum,na.rm=TRUE))
 png("histogramtotalsteps.R")
 hist(total_steps_per_day$x,xlab="Total Steps per Day")
 dev.off()
+###Calculate mean and median of total steps
+mean_total_steps<-mean(total_steps_per_day$x)
+mean_total_steps
+median_total_steps<-median(total_steps_per_day$x)
+median_total_steps
+
+##What is the average daily activity pattern?
+###Calculating average number of steps per interval
+mean_steps_per_interval<-with(activity,aggregate(steps,list(interval),mean,na.rm=TRUE))
+png("plotaverageinterval.png")
+with(mean_steps_per_interval,plot(Group.1,x,type = "l",xlab="Interval",
+                                  ylab="Average Number of Steps"))
+dev.off()
+###Getting the interval with the most number of average steps
+max_index<-which.max(mean_steps_per_interval$x)
+max_interval<-mean_steps_per_interval[max_index,1]
+max_interval #Interval 835 has the most average number of steps accross all days
+
